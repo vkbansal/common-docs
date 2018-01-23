fa8c68f83c201e5426c257ed71b806845e91d276
Vivek Kumar Bansal <vkb0310@gmail.com>
2018-01-23 12:41:12 +0530

    initial commit

---
+++ components/ReactComponentApi.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import glamorous from 'glamorous';
+import { ComponentDoc } from 'react-docgen-typescript/lib';
+import { Illuminate } from 'react-illuminate';
+
+const Heading = glamorous.h3({
+    fontWeight: 'bold',
+    color: '#e94949'
+});
+
+export interface ReactComponentApiProps {
+    api: ComponentDoc;
+    lang: string;
+}
+
+export class ReactComponentApi extends React.Component<ReactComponentApiProps> {
+    shouldComponentUpdate() {
+        return false;
+    }
+
+    renderProps(key: string, i: number) {
+        const prop = this.props.api.props[key];
+        const { description } = prop;
+        const [desc, example] = description.split('@example');
+
+        return (
+            <div key={i}>
+                <Heading>
+                    {`props.${key}${prop.required ? '' : '?'}: ${prop.type.name.split(' | ')[0]}`}
+                    {prop.defaultValue && <>{` (default: ${prop.defaultValue.value})`}</>}
+                </Heading>
+                <div dangerouslySetInnerHTML={{ __html: desc }} />
+                {example && <Illuminate lang={this.props.lang}>{example.trim()}</Illuminate>}
+            </div>
+        );
+    }
+
+    render() {
+        return Object.keys(this.props.api.props).map(this.renderProps);
+    }
+}
