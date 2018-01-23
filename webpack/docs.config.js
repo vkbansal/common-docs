946c7820d765ef708bc119f46b73ee6123f8f391
Vivek Kumar Bansal <vkb0310@gmail.com>
2018-01-23 15:31:42 +0530

    add webpack config

---
+++ webpack/docs.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+
+function docsConfig({ context, PROD, pathname, pageTitle }) {
+    const config = {
+        context,
+        entry: ['./index.tsx'],
+        output: {
+            filename: DEV ? 'bundle.js' : 'bundle.[hash].js',
+            path: path.resolve(context, '../public'),
+            publicPath: DEV ? '/' : `/${pathname}/`,
+            hashDigestLength: 6,
+            sourceMapFilename: 'bundle.js.map'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: 'ts-loader',
+                            options: {
+                                compilerOptions: {
+                                    declaration: false
+                                }
+                            }
+                        }
+                    ],
+                    include: [
+                        path.resolve(context, '../src'),
+                        path.resolve(context),
+                        path.resolve(context, '../node_modules/@vkbansal/scripts')
+                    ]
+                },
+                {
+                    test: /\.css$/,
+                    use: 'glamor-loader'
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: path.resolve(context, '../node_modules/@vkbansal/scripts/template.html'),
+                inject: true,
+                title: pathname,
+                filename: 'index.html'
+            })
+        ],
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        }
+    };
+
+    !PROD && (config.devtool = 'source-map');
+
+    PROD &&
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env': {
+                    NODE_ENV: JSON.stringify('production')
+                }
+            }),
+            new MinifyPlugin(),
+            new CompressionPlugin({
+                asset: '[path][query]',
+                test: /\.js$|\.css/
+            })
+        );
+
+    return config;
+}
+
+module.exports = docsConfig;
